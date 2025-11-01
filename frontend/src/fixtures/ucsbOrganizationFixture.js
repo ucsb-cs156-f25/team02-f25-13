c6446b38d8863e9b3b0b27c37fb33a178591ae51
Jackson Cao <jacksoncao@ucsb.edu>
2025-11-01 14:42:20 -0700

    added id to the fixture

--- frontend/src/fixtures/ucsbOrganizationFixture.js
+++ frontend/src/fixtures/ucsbOrganizationFixture.js
@@ -1,6 +1,7 @@
 const UCSBOrganizationFixtures = {
   oneOrganization: [
     {
+      id: 1,
       orgCode: "MTG",
       orgTranslationShort: "MTG@UCSB",
       orgTranslation: "Magic: The Gathering at UCSB",
@@ -10,16 +11,19 @@ const UCSBOrganizationFixtures = {
 
   threeOrganizations: [
     {
+      id: 1,
       orgCode: "GG",
       orgTranslationShort: "Gaucho Gaming",
       orgTranslation: "UCSB Gaucho Gaming",
       inactive: false,
     },    {
+      id: 2,
       orgCode: "TTG",
       orgTranslationShort: "TTG@UCSB",
-      orgTranslation: "UCSB Tabletop G  aming",
+      orgTranslation: "UCSB Tabletop Gaming",
       inactive: false,
     },    {
+      id: 3,
       orgCode: "CIA",
       orgTranslationShort: "Central Intelligence Agency",
       orgTranslation: "Central Intelligence Agency",
