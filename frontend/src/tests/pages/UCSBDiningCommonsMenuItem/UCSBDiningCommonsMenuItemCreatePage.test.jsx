6649cff81552ca421117e7b4e7887dbffe663c61
Tim Chung <yoonseo642@ucsb.edu>
2025-11-04 22:17:29 -0800

    yscreatepagetestsstories

--- frontend/src/tests/pages/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemCreatePage.test.jsx
+++ frontend/src/tests/pages/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemCreatePage.test.jsx
@@ -83,7 +83,7 @@ describe("UCSBDiningCommonsMenuItemCreatePage tests", () => {
 
     await waitFor(() => {
       expect(
-        screen.getByTestId("UCSBDiningCommonsMenuItemForm-diningCommonsCode"),
+        screen.getByTestId("UCSBDiningCommonsMenuItemForm-name"),
       ).toBeInTheDocument();
     });
 
