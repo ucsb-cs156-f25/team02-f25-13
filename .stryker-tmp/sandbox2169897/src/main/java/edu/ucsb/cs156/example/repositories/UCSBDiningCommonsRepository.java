1f1e8744ff43c4d1deafc771c31f7467360e70a8
Tim Chung <yoonseo642@ucsb.edu>
2025-11-04 18:52:07 -0800

    ys-add placeholder page

---
+++ .stryker-tmp/sandbox2169897/src/main/java/edu/ucsb/cs156/example/repositories/UCSBDiningCommonsRepository.java
@@ -0,0 +1,9 @@
+package edu.ucsb.cs156.example.repositories;
+
+import edu.ucsb.cs156.example.entities.UCSBDiningCommons;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/** The UCSBDiningCommonsRepository is a repository for UCSBDiningCommons entities */
+@Repository
+public interface UCSBDiningCommonsRepository extends CrudRepository<UCSBDiningCommons, String> {}
