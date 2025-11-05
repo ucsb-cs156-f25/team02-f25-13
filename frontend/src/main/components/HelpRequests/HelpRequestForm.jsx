eb364aa7a8d4bf0eb779629ecbb3ca3b4344f965
Hao Ding <hao_ding@ucsb.edu>
2025-11-05 12:44:41 -0800

    Fixed submit button testid issue - added a testid for submit button

--- frontend/src/main/components/HelpRequests/HelpRequestForm.jsx
+++ frontend/src/main/components/HelpRequests/HelpRequestForm.jsx
@@ -141,7 +141,7 @@ function HelpRequestForm({
         </Form.Control.Feedback>
       </Form.Group>
 
-      <Button type="submit">{buttonLabel}</Button>
+      <Button type="submit" data-testid={testIdPrefix + "-submit"}>{buttonLabel}</Button>
       <Button
         variant="Secondary"
         onClick={() => navigate(-1)}
