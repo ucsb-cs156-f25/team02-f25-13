dce4a4da3f32f6ece89182f3325af131550b3b7c
Yulin Yu <y_yu@ucsb.edu>
2025-11-06 13:44:26 -0800

    yy - relace recRequest create placeholder with real create page logic

--- frontend/src/main/pages/RecommendationRequest/RecommendationRequestCreatePage.jsx
+++ frontend/src/main/pages/RecommendationRequest/RecommendationRequestCreatePage.jsx
@@ -1,11 +1,51 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import RecommendationRequestForm from "main/components/RecommendationRequest/RecommendationRequestForm";
+import { Navigate } from "react-router";
+import { useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
-export default function RecommendationRequestCreatePage() {
-  // Stryker disable all : placeholder for future implementation
+export default function RecommendationRequestCreatePage({ storybook = false }) {
+  const objectToAxiosParams = (recommendationRequest) => ({
+    url: "/api/recommendationrequest/post",
+    method: "POST",
+    params: {
+      requesteremail: recommendationRequest.requesteremail,
+      professoremail: recommendationRequest.professoremail,
+      explanation: recommendationRequest.explanation,
+      daterequested: recommendationRequest.daterequested,
+      dateneeded: recommendationRequest.dateneeded,
+      done: recommendationRequest.done,
+    },
+  });
+
+  const onSuccess = (recommendationRequest) => {
+    toast(
+      `New restaurant Created - id: ${recommendationRequest.id} requester email: ${recommendationRequest.requesteremail}`,
+    );
+  };
+
+  const mutation = useBackendMutation(
+    objectToAxiosParams,
+    { onSuccess },
+    // Stryker disable next-line all : hard to set up test for caching
+    ["/api/recommendationrequest/all"], // mutation makes this key stale so that pages relying on it reload
+  );
+
+  const { isSuccess } = mutation;
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  };
+
+  if (isSuccess && !storybook) {
+    return <Navigate to="/recommendationrequest" />;
+  }
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Create page not yet implemented</h1>
+        <h1>Create New Recommendation Request</h1>
+        <RecommendationRequestForm submitAction={onSubmit} />
       </div>
     </BasicLayout>
   );
